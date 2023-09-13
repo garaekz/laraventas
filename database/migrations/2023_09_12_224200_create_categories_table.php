b4ca45d3fd5a54e27ab203a3a10edfebe1e10414
David Garay <garaekz@gmail.com>
2023-09-13 08:21:07 -0700

    Add status to category model

--- database/migrations/2023_09_12_224200_create_categories_table.php
+++ database/migrations/2023_09_12_224200_create_categories_table.php
@@ -13,8 +13,7 @@ return new class () extends Migration {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('status')
-                ->default(true);
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
