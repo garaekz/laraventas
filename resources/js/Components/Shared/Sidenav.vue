780285ce3988b9ffbba721f791c2fc7d23159e70
David Garay <garaekz@gmail.com>
2023-09-12 17:01:07 -0700

    Add units link to sidenav

--- resources/js/Components/Shared/Sidenav.vue
+++ resources/js/Components/Shared/Sidenav.vue
@@ -72,6 +72,12 @@ const props = defineProps({
                                         <a href="#"
                                             class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Categorías</a>
                                     </li>
+                                    <li>
+                                        <Link :href="route('units.index')"
+                                            class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                            Unidades de medida
+                                        </Link>
+                                    </li>
                                 </ul>
                             </DisclosurePanel>
                         </transition>
